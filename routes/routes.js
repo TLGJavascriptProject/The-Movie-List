c1fbbdab7ba35149ebb5aa842b79a57b752ee7f0
Aaron Prince <aprin418@gmail.com>
2022-03-21 15:38:42 -0700

    random stuff

---
+++ routes/routes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const { requiresAuth } = require("express-openid-connect");
+
+// const routes_controller = require('../controllers/routes_controller');
+const router = express.Router();
+
+module.exports = router;
