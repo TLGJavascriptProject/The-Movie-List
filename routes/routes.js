52c0600af1a94965401750bbd241b86aacd2b63f
Leonard Box <lfbox7@yahoo.com>
2022-03-22 10:15:52 -0500

    details route added and tmdb.js update

--- routes/routes.js
+++ routes/routes.js
@@ -6,9 +6,21 @@ const tmdb = require('../controllers/tmdb')
 
 // const routes_controller = require('../controllers/routes_controller');
 const router = express.Router();
-const uri = 'https://api.themoviedb.org/3/movie/now_playing?api_key=97bec1ad4a2b74922d6d176e79edf55c&language=en-US&page=1';
+const uri = 'https://api.themoviedb.org/3/movie/';
+
+router.get('/details', async(req, res) => {
+    const id = req.query.id;
+    const get_movie = await tmdb.get_movie(`${uri}${id}?api_key=${process.env.API_KEY}&language=en-US`);
+    const title = get_movie.title;
+    res.render('details', {
+        title: title,
+        movie: get_movie
+    });
+});
+
+
 router.get('/', async(req, res) => {
-    const now_playing = await tmdb.get_now_playing(uri)
+    const now_playing = await tmdb.get_now_playing(`${uri}now_playing?api_key=${process.env.API_KEY}&language=en-US&page=1`);
     res.render('index',{
         title: 'Homepage',
         movies: now_playing
